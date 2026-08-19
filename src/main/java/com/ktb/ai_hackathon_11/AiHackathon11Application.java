5850291de0f97a023cb51798e12c1cde17cf6248
jisoo <0801ljs@naver.com>
2026-08-19 09:25:29 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/com/ktb/ai_hackathon_11/AiHackathon11Application.java
@@ -0,0 +1,13 @@
+package com.ktb.ai_hackathon_11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiHackathon11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiHackathon11Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ktb/ai_hackathon_11/AiHackathon11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktb.ai_hackathon_11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiHackathon11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
